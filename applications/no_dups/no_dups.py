9977a160b763bd9aaa80565802a7d399f8a5e505
numbers0580 <koreankid18@yahoo.com>
2020-10-01 02:55:59 -0600

    About halfway done

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,5 +1,16 @@
 def no_dups(s):
     # Your code here
+    cache = {}
+    word = s.split() # parse to individual words
+
+    temp = "" # like word_count, create an empty string to use in for-loop
+
+    for c in word:
+        # parsed each word to individual characters (not testing for symbols or capital letters, this time)
+        if c not in cache:
+            cache[c] = 1 # Just giving c some arbitrary value so it'll be found on duplicates
+            temp += f"{c} " # Appending only the first iteration of the word found. Left a trailing space, but will strip last space after loop
+    return temp.strip()
 
 
 
--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,6 +1,36 @@
 def word_count(s):
     # Your code here
+    symbols = ['"', ',', '.', ':', ';', '-', '+', '=', '/', '\\', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
 
+    cache = {}
+
+    if s == "":
+        return {}
+    else:
+        for x in s.lower().split():
+            # Went to w3schools.com to test.
+            # For the 'Hello, my cat. And my cat doesn\'t say "hello" back.' string...
+            # Found x = ["hello,", "my", "cat.", "and", "my", "cat", "doesn't", "say", "\"hello\"", "back."]
+
+            temp = ""
+            # Getting each character of each word and only appending non-symbols to the temp string.
+            # Once the non-symbol word has been fully concatenated, we'll store that back to x
+            for c in x:
+                if c not in symbols:
+                    temp += c
+            x = temp
+
+            if x in cache:
+                # Found a duplicate word, increment it's count
+                cache[x] += 1
+            elif x == "" or x == " ":
+                # Empty string
+                break
+            else:
+                # Found a new word
+                cache[x] = 1
+
+        return cache
 
 
 if __name__ == "__main__":
